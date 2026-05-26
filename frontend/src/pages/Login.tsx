b7999423dce3a567c689088edd74f1871deafddf
TedPasquiet <pasquietted@gmail.com>
2026-05-26 13:42:23 +0200

    feat: main render of login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,14 +1,14 @@
 import { Header } from "../components/header";
+import Frame from "../../public/Frame 3.png";
 
 export function Login() {
   return (
-    <main>
+    <main className="login-page">
       <Header />
-      <span className="title-login">
-        <p>
-        Tu veux partager un fichier ?
-        </p>
-      </span>
+      <div className="login-content">
+        <p className="title-text">Tu veux partager un fichier ?</p>
+        <img src={Frame} alt="Upload Icon" className="upload-icon" />
+      </div>
     </main>
   );
 }
