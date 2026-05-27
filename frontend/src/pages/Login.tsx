41b466f9e5edd7731a4103875387ae0fa0cc1cac
TedPasquiet <pasquietted@gmail.com>
2026-05-27 17:40:06 +0200

    feat: finalisation of frontPage

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Header } from "../components/header";
 import { AuthModal } from "../components/AuthModal";
+import { Footer } from "../components/footer";
+
 export function Login() {
   const [modalOpen, setModalOpen] = useState(false);
 
@@ -11,6 +13,7 @@ export function Login() {
         <p className="title-text">Tu veux partager un fichier ?</p>
         <img src="/Frame 3.png" alt="Upload Icon" className="supload-icon" />
       </div>
+      <Footer />
       <AuthModal isOpen={modalOpen} onClose={() => setModalOpen(false)} />
     </main>
   );
